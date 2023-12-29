f470be1f5b19e737982ec105df29f6e0281832bb
Prasad More <prasadmoremain@gmail.com>
2023-12-29 16:58:02 +0530

    Completed with Assigned Exam Center Randomly

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 let users = []
 
+
+let regUsers = document.getElementById('registeredUser')
+
+
+let records = () => {
+    regUsers.innerHTML=''
+
+    for(let i=0;i<users.length;i++){
+    let div = document.createElement('div')
+    let name = document.createElement('div')
+    let email = document.createElement('div')
+    let examC = document.createElement('div')
+
+    div.classList.add('users')
+
+    name.innerText = users[i].name;
+    email.innerText = users[i].email
+    examC.innerText = users[i].center;
+
+    regUsers.appendChild(div)
+    div.appendChild(name)
+    div.appendChild(email)
+    div.appendChild(examC)    
+    
+    }
+
+}
+
+
 let examination = () =>{
 
     let name = document.getElementById('name')
@@ -9,15 +38,24 @@ let examination = () =>{
         return data.email == email.value
     })
 
+    let centers = ['pune','mumbai','delhi','nashik','himachal','mp','up','lucknow','bihar','jammu']
+    
+    let random = Math.random()*10;
+    let num = Math.trunc(random);
 
+    for(let i=0;i<10;i++){
+        center = centers[num]
+    }
 
     let examUsers = {
         'name': name.value,
-        'email': email.value
+        'email': email.value,
+        'center': center
     }
 
     if(ifUserExist.length == 0){
         users.push(examUsers)
+        records()
     } 
     else {
         window.alert('User is Already Exist!')
